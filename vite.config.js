2bd08232ba5001563728fa1af3c74024d96d788b
Abhishek Jaiswar <devabhishekjaiswar@gmail.com>
2026-05-14 23:18:23 +0530

    config: add prettier and do configs

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,17 @@
 import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
-import path from "path";
-import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
-
-const __filename = fileURLToPath(import.meta.url); // Get the resolved path to the file
-const __dirname = path.dirname(__filename); // Get the name of the directory
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    tsconfigPaths(),
     babel({ presets: [reactCompilerPreset()] }),
   ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
   server: {
     port: 3000,
   },
